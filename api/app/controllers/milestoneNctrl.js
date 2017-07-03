ff984c195d7b5a297cb869dfa710b9404c14fade
kalyani-parentof <kalyani@parentof.com>
2017-07-03 10:10:02 +0530

    Create milestoneNctrl.js

---
+++ api/app/controllers/milestoneNctrl.js
@@ -0,0 +1,87 @@
+/**
+ * Created by parentof 6 on 20-06-2017.
+ */
+var Milestone = require('mongoose').model('milestone');
+
+
+exports.post = function(req, res){
+    var milestone = new Milestone(req.body);
+    milestone.save(function(err){
+        if(err){
+            res.error(err)
+        }
+        else{
+            res.success(milestone)
+        }
+    })
+}
+
+
+exports.get = function(req, res){
+    Milestone.find({}, function(err, data){
+        if(err){
+            res.error(err)
+        }
+        else{
+            res.success(data)
+        }
+    })
+}
+
+
+exports.update = function(req, res){
+    var milestoneReq = req.body;
+    Milestone.findOne({_id: milestoneReq._id}, function(err, milestone){
+        milestone.name = milestoneReq.name;
+        milestone.save(function(err){
+            if(err){
+                res.error(err)
+            }
+            else{
+                res.success(milestone)
+            }
+        })
+    })
+}
+exports.getAll = function(req, res){
+    Milestone.find({}, function(err, data){
+        if(err){
+            res.error(err)
+        }
+        else{
+            res.success(data)
+        }
+    })
+}
+
+exports.deleteMilestone= function(req, res){
+
+    var id = req.params.id;
+    console.log(id);
+
+    Milestone.remove({_id: id}, function(err,data){
+
+        if(err){
+            res.error(err)
+        }
+        else{
+            res.success(data)
+        }
+
+    })
+
+
+
+    res.success("deleted successfully")
+}
+/*
+exports.findByIdAndRemove(req.params.id, function (err, milestone) {
+    // We'll create a simple object to send back with a message and the id of the document that was removed
+    // You can really do this however you want, though.
+    var response = {
+        message: "Todo successfully deleted",
+        id: milestone._id
+    };
+    res.send(response);
+});
+*/
