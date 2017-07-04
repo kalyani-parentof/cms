0503fd14e2486f8afbdbfbc4008c0d7508ea0126
kalyani-parentof <kalyani@parentof.com>
2017-07-04 11:55:59 +0530

    Create taxonomyCategoryCtrl.js

---
+++ api/app/controllers/taxonomyCategoryCtrl.js
@@ -0,0 +1,44 @@
+
+var TaxonomyCategory = require('mongoose').model('taxonomyCategory');
+
+
+exports.post = function(req, res){
+    var taxonomyCategory = new TaxonomyCategory(req.body);
+    taxonomyCategory.save(function(err){
+        if(err){
+            res.error(err)
+        }
+        else{
+            res.success(taxonomyCategory)
+        }
+    })
+}
+
+
+exports.get = function(req, res){
+    TaxonomyCategory.find({}, function(err, data){
+        if(err){
+            res.error(err)
+        }
+        else{
+            res.success(data)
+        }
+    })
+}
+
+
+exports.update = function(req, res){
+    var taxonomyCategoryReq = new TaxonomyCategory(req.body);
+    TaxonomyCategory.findOne({_id: taxonomyCategoryReq._id}, function(err, taxonomyCategory){
+        taxonomyCategory.name = taxonomyCategoryReq.name
+        taxonomyCategory.description = taxonomyCategoryReq.description
+        taxonomyCategory.save(function(err){
+            if(err){
+                res.error(err)
+            }
+            else{
+                res.success(taxonomyCategory)
+            }
+        })
+    })
+}
